6e684ef17379c20acc84f28d0e216206050cd56b
Jose Luis Martinez <ivonnics@protonmail.com>
2018-12-02 14:35:52 -0500

    Add files via upload

---
+++ HWL4_NEW_Tone.ino
@@ -0,0 +1,21 @@
+void setup() {
+ Serial.begin(9600);
+pinMode(11, OUTPUT);
+ 
+}
+
+void loop() {
+
+for (int i=0; i <= 4; i++){
+     
+   
+
+  Serial.println("Tone sending...");
+ //tone(pin,frequency,durantion);
+ tone(11, 200,1500);  //play note for 1500ms
+ delay(1500);
+ Serial.println("Tone done");
+ delay(500);
+} 
+while(1);
+}
